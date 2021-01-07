52190d144b5559683af8a37b274319219714e8bd
mine6ra <mine6ra405@gmail.com>
2021-01-07 19:12:56 +0900

    add zsh and prezto

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
-for name in .*; do
-    if [ ! -d "$name" ]; then
-        [ "$name" = ".DS_Store" ] && continue
+for name in *; do
+    [ "$name" = "install.sh" ] && continue
+    [ "$name" = "LICENSE" ] && continue
+    [ "$name" = "README.md" ] && continue
+    [ "$name" = ".DS_Store" ] && continue
 
-        # 上書きはしない
-        target="$HOME/$name"
-        if [ ! -e "$target" ]; then
-            ln -s "$PWD/$name" "$target"
-            echo "Created $name"
-        fi
+    # 上書きはしない
+    target="$HOME/.$name"
+    if [ ! -e "$target" ]; then
+        ln -s "$PWD/$name" "$target"
+        echo "Created $name"
     fi
 done
